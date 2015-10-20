9db632026495af77779a0d5ed84563951bcdf66d
Jeremy Boggs <jeremy@clioweb.org>
2015-10-20 18:56:18 -0400

    Get compass working.

--- config.rb
+++ config.rb
@@ -1,9 +1,9 @@
-require 'susy' #Susy framework
+require 'susy' #Susy framework
 
 http_path = "/"
-css_dir = "/"
+css_dir = "."
 sass_dir = "_sass"
 images_dir = "images"
 javascripts_dir = "javascripts"
-output_style  = :expanded
+output_style  = :expanded
 line_comments = false
