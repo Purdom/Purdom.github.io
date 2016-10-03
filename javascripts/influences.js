f205ace74eedab5ae4f969922fa56d364fad93ee
Eric Rochester <erochest@virginia.edu>
2016-10-02 22:09:17 -0400

    Updates site.

--- javascripts/influences.js
+++ javascripts/influences.js
@@ -5,13 +5,13 @@
  * - orange      = influenced-by for P's reading.
  */
 
-"use strict";
+'use strict';
 
-var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
 
-var height = 500;
+var height = 600;
 var width = 960;
 var strokeWidth = 3;
 var radius = 12;
@@ -23,8 +23,7 @@ var linkDistance = 100;
 withPostLD(function (expanded) {
   var gb = new GraphBuilder(expanded);
   window.gbuilder = gb;
-  gb.buildNodes();
-  gb.buildLinks();
+  gb.buildGraph();
 
   var gv = new GraphViewer(gb.nodes, gb.links);
   window.gviewer = gv;
@@ -37,165 +36,135 @@ var GraphBuilder = (function () {
     _classCallCheck(this, GraphBuilder);
 
     this.nodes = [];
+    this.nodesSet = new Map();
     this.links = [];
+    this.linksSet = new Map();
 
     this.s = byS(expanded);
     this.p = byP(expanded);
     this.o = byO(expanded);
-
-    this.nodesSet = new Map();
-    this.linksSet = new Map();
   }
 
   _createClass(GraphBuilder, [{
-    key: "buildNodes",
-    value: function buildNodes() {
+    key: 'buildGraph',
+    value: function buildGraph() {
+      this.buildRead();
+      this.buildInspiredByP();
+      this.buildInspiredByDb();
+    }
+  }, {
+    key: 'buildRead',
+    value: function buildRead() {
       var _this = this;
 
-      this.p.get(creator).forEach(function (creatorP) {
-        var objId = creatorP[creator][0]["@id"],
-            creatorNodes = _this.s.get(objId);
-
-        if (creatorNodes != null) {
-          creatorNodes.forEach(function (node) {
-            return _this.addNode(node);
-          });
-        }
+      this.p.get(leadTo).forEach(function (node) {
+        var subj = _this.getCreator(node);
+        // console.log('leadTo', source, subj);
+        node[leadTo].forEach(_this.buildLink(subj, 'source', 'target', 'read'));
       });
     }
   }, {
-    key: "addNode",
-    value: function addNode(node) {
+    key: 'buildInspiredByP',
+    value: function buildInspiredByP() {
+      var _this2 = this;
+
+      this.p.get(inspiredBy).forEach(function (node) {
+        var obj = _this2.getCreator(node);
+        // console.log('inspiredBy', target, obj);
+        node[inspiredBy].forEach(_this2.buildLink(obj, 'target', 'source', 'read', 'infl-purdom'));
+      });
+    }
+  }, {
+    key: 'buildInspiredByDb',
+    value: function buildInspiredByDb() {}
+  }, {
+    key: 'buildLink',
+    value: function buildLink(a, propa, propb, clsa) {
+      var _this3 = this;
+
+      var clsb = arguments.length <= 4 || arguments[4] === undefined ? null : arguments[4];
+
+      clsb = clsb === null ? clsa : clsb;
+      var aid = a["@id"];
+
+      return function (bnode) {
+        var b = _this3.getCreator(_this3.s.get(bnode["@id"])[0]),
+            bid = b["@id"],
+            link;
+
+        if (aid === bid) {
+          return;
+        }
+
+        if (!_this3.linkSeen(aid, bid)) {
+          link = {};
+          link[propa] = _this3.addNode(a, clsa);
+          link[propb] = _this3.addNode(b, clsb);
+          link['@class'] = clsb;
+
+          _this3.links.push(link);
+          _this3.seeLink(aid, bid);
+        }
+      };
+    }
+  }, {
+    key: 'addNode',
+    value: function addNode(node, cls) {
       var i;
       var nodeId = node["@id"];
-      if (!this.nodesSet.has(nodeId)) {
+
+      if (this.nodesSet.has(nodeId)) {
+        i = this.nodesSet.get(nodeId);
+      } else {
         i = this.nodes.length;
         this.nodes.push(node);
         this.nodesSet.set(nodeId, i);
-      } else {
-        i = this.nodesSet.get(nodeId);
+        if (cls !== null) {
+          node["@class"] = cls;
+        }
       }
+
       return i;
     }
   }, {
-    key: "buildLinks",
-    value: function buildLinks() {
-      var _this2 = this;
-
-      this.p.get(leadTo).forEach(function (source) {
-        var subj = getCreator(_this2.s, source);
-        // console.log('leadTo', source, subj);
-        source[leadTo].forEach(_this2.buildTargets(subj));
-      });
-      this.p.get(inspiredBy).forEach(function (target) {
-        var obj = getCreator(_this2.s, target);
-        // console.log('inspiredBy', target, obj);
-        target[inspiredBy].forEach(_this2.buildSources(obj));
-      });
-    }
-  }, {
-    key: "buildSources",
-    value: function buildSources(obj) {
-      var _this3 = this;
-
-      var oId = obj["@id"];
-
-      return function (sourceId) {
-        var creatorId, subject, sId, link;
-        var source = _this3.s.get(sourceId["@id"])[0];
-        var sourceType = getTypeOf(source);
-        // console.log('subject', sourceId, source);
-
-        if (sourceType === bibframe("Work") || sourceType === bibframe('BlogPost')) {
-          creatorId = source[creator];
-          if (creatorId == null) {
-            return;
-          }
-          subject = _this3.s.get(creatorId[0]["@id"])[0];
-        } else {
-          subject = source;
-        }
-
-        sId = subject["@id"];
-        if (sId === oId) {
-          return;
-        }
-
-        if (!_this3.linkSeen(sId, oId)) {
-          link = {
-            source: _this3.addNode(subject),
-            target: _this3.nodesSet.get(oId)
-          };
-          _this3.links.push(link);
-          _this3.seeLink(sId, oId);
-        }
-      };
-    }
-  }, {
-    key: "buildTargets",
-    value: function buildTargets(subject) {
-      var _this4 = this;
-
-      var sId = subject["@id"];
-
-      return function (targetId) {
-        var creatorId, obj, oId, link;
-        var target = _this4.s.get(targetId["@id"])[0];
-        // console.log('target', targetId, target);
-
-        if (getTypeOf(target) === bibframe("Work")) {
-          creatorId = target[creator];
-          if (creatorId == null) {
-            return;
-          }
-          obj = _this4.s.get(creatorId[0]["@id"])[0];
-        } else {
-          obj = target;
-        }
-
-        oId = obj["@id"];
-        if (sId === oId) {
-          return;
-        }
-
-        if (!_this4.linkSeen(sId, oId)) {
-          link = {
-            source: _this4.nodesSet.get(sId),
-            target: _this4.nodesSet.get(oId)
-          };
-          _this4.links.push(link);
-          _this4.seeLink(sId, oId);
-        }
-      };
-    }
-  }, {
-    key: "seeLink",
+    key: 'seeLink',
     value: function seeLink(s, o) {
-      if (!this.linksSet.has(s)) {
-        this.linksSet.set(s, new Set());
+      var nodes = [s, o];
+      nodes.sort();
+
+      if (!this.linksSet.has(nodes[0])) {
+        this.linksSet.set(nodes[0], new Set());
       }
-      this.linksSet.get(s).add(o);
+      this.linksSet.get(nodes[0]).add(nodes[1]);
     }
   }, {
-    key: "linkSeen",
+    key: 'linkSeen',
     value: function linkSeen(s, o) {
-      var sSet = this.linksSet.get(s);
-      return sSet != undefined && sSet.has(o);
+      var nodes = [s, o],
+          sSet;
+      nodes.sort();
+
+      var sSet = this.linksSet.get(nodes[0]);
+      return sSet != undefined && sSet.has(nodes[1]);
+    }
+  }, {
+    key: 'getCreator',
+    value: function getCreator(obj) {
+      var t = getTypeOf(obj),
+          c;
+
+      if ((t === bibframe('Work') || t === bibframe('BlogPost')) && obj[creator] != null) {
+        c = this.s.get(obj[creator][0]["@id"])[0];
+      } else {
+        c = obj;
+      }
+
+      return c;
     }
   }]);
 
   return GraphBuilder;
 })();
-
-function getCreator(sindex, obj) {
-  var c;
-  if (getTypeOf(obj) === bibframe('Work')) {
-    c = sindex.get(obj[creator][0]["@id"])[0];
-  } else {
-    c = obj;
-  }
-  return c;
-}
 
 var GraphViewer = (function () {
   function GraphViewer(nodes, links) {
@@ -206,19 +175,23 @@ var GraphViewer = (function () {
   }
 
   _createClass(GraphViewer, [{
-    key: "init",
+    key: 'init',
     value: function init() {
       this.force = d3.layout.force().charge(charge).linkDistance(linkDistance).size([width, height]);
       this.svg = d3.select("#tree").append("svg").attr("width", width).attr("height", height);
     }
   }, {
-    key: "start",
+    key: 'start',
     value: function start() {
-      var _this5 = this;
+      var _this4 = this;
 
       this.force.nodes(this.nodes).links(this.links).start();
-      this.linkNodes = this.svg.selectAll('line').data(this.links).enter().append("line").classed({ 'link': true, 'read': true });
-      this.nodeNodes = this.svg.selectAll('circle').data(this.nodes).enter().append("circle").classed({ 'node': true, 'read': true }).attr("r", radius)
+      this.linkNodes = this.svg.selectAll('line').data(this.links).enter().append("line").attr('class', function (d) {
+        return 'link ' + d["@class"];
+      });
+      this.nodeNodes = this.svg.selectAll('circle').data(this.nodes).enter().append("circle").attr('class', function (d) {
+        return 'node ' + d["@class"];
+      }).attr("r", radius)
       // .style("fill", color)
       // .on("mouseover", () => this.mouseover())
       .call(this.force.drag);
@@ -226,11 +199,11 @@ var GraphViewer = (function () {
         return getLabel(d);
       });
       this.force.on("tick", function () {
-        return _this5.tick();
+        return _this4.tick();
       });
     }
   }, {
-    key: "tick",
+    key: 'tick',
     value: function tick() {
       this.linkNodes.attr("x1", function (d) {
         return d.source.x;
@@ -248,7 +221,7 @@ var GraphViewer = (function () {
       });
     }
   }, {
-    key: "mouseover",
+    key: 'mouseover',
     value: function mouseover(obj) {}
   }]);
 
