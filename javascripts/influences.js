18b5925a2c9835da198df82856026b116652a711
Eric Rochester <erochest@virginia.edu>
2016-10-02 20:39:04 -0400

    Updates site.

--- javascripts/influences.js
+++ javascripts/influences.js
@@ -1,44 +1,256 @@
-[
-  {
-    name: "China_Miéville",
-    size: 3000,
-      imports: [J._G._Ballard, Michael_Moorcock]
-},
+/*
+ * Legend:
+ * - dark green  = those P's read;
+ * - light green = influenced-by according to dbpedia;
+ * - orange      = influenced-by for P's reading.
+ */
 
-  {
-    name: "J._G._Ballard",
-    size: 3000,
-      imports: [Marshall_McLuhan, William_S._Burroughs]
-},
+"use strict";
 
-{
-  name: "Michael_Moorcock",
-  size: 3000,
-  imports: [J._G._Ballard, William_S._Burroughs]
-},
+var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
 
-{
-  :if expand("%") == ""|browse confirm w|else|confirm w|endif
-    William_S._Burroughs,
-  size: 3000,
-  imports: [Jean-Paul_Satre]
-},
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-{
-  J._G._Ballard,
-  size: 3000,
-  imports: [William_S._Burroughs]
-},
+var height = 500;
+var width = 960;
+var strokeWidth = 3;
+var radius = 12;
+var charge = -400;
+var linkDistance = 100;
 
-{
-  Jean-Paul_Satre,
-  size: 3000,
-  imports: [Simone_de_Beauvoir]
-},
+// get posts.json
+// pull influences according to what p's read.
+withPostLD(function (expanded) {
+  var gb = new GraphBuilder(expanded);
+  window.gbuilder = gb;
+  gb.buildNodes();
+  gb.buildLinks();
 
-{
-  Simone_de_Beauvoir,
-  size: 3000,
-  imports: [Jean-Paul_Satre]
-},
+  var gv = new GraphViewer(gb.nodes, gb.links);
+  window.gviewer = gv;
+  gv.init();
+  gv.start();
+});
 
+var GraphBuilder = (function () {
+  function GraphBuilder(expanded) {
+    _classCallCheck(this, GraphBuilder);
+
+    this.nodes = [];
+    this.links = [];
+
+    this.s = byS(expanded);
+    this.p = byP(expanded);
+    this.o = byO(expanded);
+
+    this.nodesSet = new Map();
+    this.linksSet = new Map();
+  }
+
+  _createClass(GraphBuilder, [{
+    key: "buildNodes",
+    value: function buildNodes() {
+      var _this = this;
+
+      this.p.get(creator).forEach(function (creatorP) {
+        var objId = creatorP[creator][0]["@id"],
+            creatorNodes = _this.s.get(objId);
+
+        if (creatorNodes != null) {
+          creatorNodes.forEach(function (node) {
+            return _this.addNode(node);
+          });
+        }
+      });
+    }
+  }, {
+    key: "addNode",
+    value: function addNode(node) {
+      var i;
+      var nodeId = node["@id"];
+      if (!this.nodesSet.has(nodeId)) {
+        i = this.nodes.length;
+        this.nodes.push(node);
+        this.nodesSet.set(nodeId, i);
+      } else {
+        i = this.nodesSet.get(nodeId);
+      }
+      return i;
+    }
+  }, {
+    key: "buildLinks",
+    value: function buildLinks() {
+      var _this2 = this;
+
+      this.p.get(leadTo).forEach(function (source) {
+        var subj = getCreator(_this2.s, source);
+        // console.log('leadTo', source, subj);
+        source[leadTo].forEach(_this2.buildTargets(subj));
+      });
+      this.p.get(inspiredBy).forEach(function (target) {
+        var obj = getCreator(_this2.s, target);
+        // console.log('inspiredBy', target, obj);
+        target[inspiredBy].forEach(_this2.buildSources(obj));
+      });
+    }
+  }, {
+    key: "buildSources",
+    value: function buildSources(obj) {
+      var _this3 = this;
+
+      var oId = obj["@id"];
+
+      return function (sourceId) {
+        var creatorId, subject, sId, link;
+        var source = _this3.s.get(sourceId["@id"])[0];
+        var sourceType = getTypeOf(source);
+        // console.log('subject', sourceId, source);
+
+        if (sourceType === bibframe("Work") || sourceType === bibframe('BlogPost')) {
+          creatorId = source[creator];
+          if (creatorId == null) {
+            return;
+          }
+          subject = _this3.s.get(creatorId[0]["@id"])[0];
+        } else {
+          subject = source;
+        }
+
+        sId = subject["@id"];
+        if (sId === oId) {
+          return;
+        }
+
+        if (!_this3.linkSeen(sId, oId)) {
+          link = {
+            source: _this3.addNode(subject),
+            target: _this3.nodesSet.get(oId)
+          };
+          _this3.links.push(link);
+          _this3.seeLink(sId, oId);
+        }
+      };
+    }
+  }, {
+    key: "buildTargets",
+    value: function buildTargets(subject) {
+      var _this4 = this;
+
+      var sId = subject["@id"];
+
+      return function (targetId) {
+        var creatorId, obj, oId, link;
+        var target = _this4.s.get(targetId["@id"])[0];
+        // console.log('target', targetId, target);
+
+        if (getTypeOf(target) === bibframe("Work")) {
+          creatorId = target[creator];
+          if (creatorId == null) {
+            return;
+          }
+          obj = _this4.s.get(creatorId[0]["@id"])[0];
+        } else {
+          obj = target;
+        }
+
+        oId = obj["@id"];
+        if (sId === oId) {
+          return;
+        }
+
+        if (!_this4.linkSeen(sId, oId)) {
+          link = {
+            source: _this4.nodesSet.get(sId),
+            target: _this4.nodesSet.get(oId)
+          };
+          _this4.links.push(link);
+          _this4.seeLink(sId, oId);
+        }
+      };
+    }
+  }, {
+    key: "seeLink",
+    value: function seeLink(s, o) {
+      if (!this.linksSet.has(s)) {
+        this.linksSet.set(s, new Set());
+      }
+      this.linksSet.get(s).add(o);
+    }
+  }, {
+    key: "linkSeen",
+    value: function linkSeen(s, o) {
+      var sSet = this.linksSet.get(s);
+      return sSet != undefined && sSet.has(o);
+    }
+  }]);
+
+  return GraphBuilder;
+})();
+
+function getCreator(sindex, obj) {
+  var c;
+  if (getTypeOf(obj) === bibframe('Work')) {
+    c = sindex.get(obj[creator][0]["@id"])[0];
+  } else {
+    c = obj;
+  }
+  return c;
+}
+
+var GraphViewer = (function () {
+  function GraphViewer(nodes, links) {
+    _classCallCheck(this, GraphViewer);
+
+    this.nodes = nodes;
+    this.links = links;
+  }
+
+  _createClass(GraphViewer, [{
+    key: "init",
+    value: function init() {
+      this.force = d3.layout.force().charge(charge).linkDistance(linkDistance).size([width, height]);
+      this.svg = d3.select("#tree").append("svg").attr("width", width).attr("height", height);
+    }
+  }, {
+    key: "start",
+    value: function start() {
+      var _this5 = this;
+
+      this.force.nodes(this.nodes).links(this.links).start();
+      this.linkNodes = this.svg.selectAll('line').data(this.links).enter().append("line").classed({ 'link': true, 'read': true });
+      this.nodeNodes = this.svg.selectAll('circle').data(this.nodes).enter().append("circle").classed({ 'node': true, 'read': true }).attr("r", radius)
+      // .style("fill", color)
+      // .on("mouseover", () => this.mouseover())
+      .call(this.force.drag);
+      this.nodeNodes.append("title").text(function (d) {
+        return getLabel(d);
+      });
+      this.force.on("tick", function () {
+        return _this5.tick();
+      });
+    }
+  }, {
+    key: "tick",
+    value: function tick() {
+      this.linkNodes.attr("x1", function (d) {
+        return d.source.x;
+      }).attr("y1", function (d) {
+        return d.source.y;
+      }).attr("x2", function (d) {
+        return d.target.x;
+      }).attr("y2", function (d) {
+        return d.target.y;
+      });
+      this.nodeNodes.attr("cx", function (d) {
+        return d.x;
+      }).attr("cy", function (d) {
+        return d.y;
+      });
+    }
+  }, {
+    key: "mouseover",
+    value: function mouseover(obj) {}
+  }]);
+
+  return GraphViewer;
+})();
---
+++ javascripts/lod.js
@@ -0,0 +1,183 @@
+'use strict';
+
+function bibframe(thing) {
+  return "http://bibframe.org/vocab/" + thing;
+}
+
+function purdom(thing) {
+  return "http://purdom.org/reading#" + thing;
+}
+
+function rdfs(thing) {
+  return "http://www.w3.org/2000/01/rdf-schema#" + thing;
+}
+
+var personType = bibframe("Person");
+var workType = bibframe("Work");
+var creator = bibframe("creator");
+var leadTo = purdom("lead_to");
+var inspiredBy = purdom("inspired_by");
+var label = rdfs("label");
+
+function getTypeOf(obj) {
+  var type = obj["@type"];
+  return type === undefined ? null : type[0];
+}
+
+function getLabel(obj) {
+  var l = obj[label];
+  if (l === undefined || l === null) {
+    l = obj["@id"];
+    if (l === undefined || l === null) {
+      l = "unknown";
+    }
+    l = "<" + l + ">";
+  } else {
+    l = l[0]["@value"];
+  }
+  return l;
+}
+
+function findType(typeName, triples) {
+  return triples.filter(function (triple) {
+    return getTypeOf(triple) === typeName;
+  });
+}
+
+function indexById(triples) {
+  var index = {};
+  triples.forEach(function (obj) {
+    var id = obj["@id"];
+    index[id] = obj;
+  });
+  return index;
+}
+
+function findLeadTo(triples) {
+  return triples.filter(function (obj) {
+    return obj[leadTo] !== undefined;
+  });
+}
+
+function findInspiredBy(graph) {
+  var expanded = [];
+  for (var i = 0; i < graph.length; i++) {
+    var node = graph[i];
+    if (node[inspiredBy] !== undefined) {
+      var inspirations = node[inspiredBy],
+          o = node["@id"];
+      for (var j = 0; j < inspirations.length; j++) {
+        expanded.push({ s: inspirations[j]["@id"], o: o });
+      }
+    }
+  }
+  return expanded;
+}
+
+function findCreateSubject(graph, sId) {
+  var subject = graph.filter(function (n) {
+    return n["@id"] === sId;
+  });
+  if (subject.length === 0) {
+    var n = {
+      "@id": sId
+    };
+    subject.push(n);
+    graph.push(n);
+  }
+  return subject;
+}
+
+function addLeadTo(n, o) {
+  var purdom_lead_to = n[leadTo];
+  if (purdom_lead_to === undefined) {
+    n[leadTo] = purdom_lead_to = [];
+  }
+  purdom_lead_to.push({
+    "@id": o
+  });
+}
+
+function expandImplicits(graph) {
+  var expanded = findInspiredBy(graph);
+
+  expanded.forEach(function (expand) {
+    var subjects = findCreateSubject(graph, expand.s);
+    subjects.forEach(function (n) {
+      addLeadTo(n, expand.o);
+    });
+  });
+
+  return graph;
+}
+
+function byType(expanded) {
+  var m = new Map();
+  expanded.forEach(function (o) {
+    pushIndex(m, o["@type"], t);
+  });
+  return m;
+}
+
+function byS(expanded) {
+  var m = new Map();
+  expanded.forEach(function (o) {
+    pushIndex(m, o["@id"], o);
+  });
+  return m;
+}
+
+function byP(expanded) {
+  var m = new Map();
+  expanded.forEach(function (o) {
+    for (var p in o) {
+      if (!p.startsWith("@")) {
+        pushIndex(m, p, o);
+      }
+    }
+  });
+  return m;
+}
+
+function byO(expanded) {
+  var m = new Map();
+  expanded.forEach(function (o) {
+    for (var p in o) {
+      if (typeof o[p] === 'string') {
+        pushIndex(m, o[p], o);
+      } else {
+        o[p].forEach(function (v) {
+          var k;
+          if (typeof v === 'string') {
+            k = v;
+          } else if (v["@id"] === undefined) {
+            k = v["@value"];
+          } else {
+            k = v["@id"];
+          }
+          pushIndex(m, k, o);
+        });
+      }
+    }
+  });
+  return m;
+}
+
+function pushIndex(m, k, v) {
+  if (!m.has(k)) {
+    m.set(k, []);
+  }
+  m.get(k).push(v);
+}
+
+function withPostLD(f) {
+  $.get('/posts.json', function (data) {
+    jsonld.expand(data, function (err, expanded) {
+      if (err !== null) {
+        console.log('ERROR', err);
+      } else {
+        f(expanded);
+      }
+    });
+  });
+}
